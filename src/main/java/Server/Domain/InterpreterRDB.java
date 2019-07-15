301c212089e24efc8150ec194f79419164a72f3f
alessiogullotti <alessio.gullotti01@universitadipavia.it>
2019-07-15 18:24:03 +0200

    Fixed some warnings

--- src/main/java/Server/Domain/InterpreterRDB.java
+++ src/main/java/Server/Domain/InterpreterRDB.java
@@ -899,7 +899,6 @@ class InterpreterRDB {
 
     public InterpreterRDB() {
         this.sessionFactory = HibernateUtil.getSessionFactory();
-        logoutAll();
         deleteAuctions();
     }
 
